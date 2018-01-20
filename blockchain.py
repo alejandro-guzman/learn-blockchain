73639417c1c21e9164673c1aca4070989fa9e56e
Alejandro Guzman <a.guillermoguzman@gmail.com>
2018-01-20 18:53:39 -0500

    typo hash

--- blockchain.py
+++ blockchain.py
@@ -26,7 +26,7 @@ class Blockchain(object):
             'timestamp': time(),
             'transactions': self.current_transactions,
             'proof': proof,
-            'previous_hash': previous_hash or self.has(self.chain[-1])
+            'previous_hash': previous_hash or self.hash(self.chain[-1])
         }
 
         # Reset current list of transactions
