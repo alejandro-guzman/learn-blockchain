4b36b1387a3bd620953bb59b4e5ea7379e64dc57
Alejandro Guzman <a.guillermoguzman@gmail.com>
2018-01-21 01:03:07 -0500

    add debug

--- server.py
+++ server.py
@@ -112,4 +112,6 @@ if __name__ == '__main__':
         port = int(sys.argv[1])
     else:
         port = 5000
-    app.run(host='0.0.0.0', port=port)
+
+    debug = True
+    app.run(host='0.0.0.0', port=port, debug=debug)
